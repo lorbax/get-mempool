1e454ff8a4e6f46671c1187bd2eb291e6b29a0a5
lorban <lorenzo.bonax@gmail.com>
2023-11-22 18:44:10 +0100

    works

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,29 @@ extern crate bitcoincore_rpc;
 use bitcoincore_rpc::{Auth, Client, Error, RpcApi};
 
 fn main_result() -> Result<(), Error> {
-    let url = "http://34.125.157.90:8333".to_string();
-    let rpc = Client::new(&url, Auth::None).unwrap();
+    let url = "http://127.0.0.1:18332".to_string();
+    let username = "username".to_string(); 
+    let password = "password".to_string(); 
+    let auth = Auth::UserPass(username, password);
 
-    let mempool = rpc.get_raw_mempool().unwrap();
-    println!("tx by `get`: {}", mempool[0]);
+    let rpc = Client::new(&url, auth)?;
+
+    let mempool = rpc.get_raw_mempool()?;
+    
+    if mempool.is_empty() {
+        println!("La mempool è vuota.");
+    } else {
+        println!("Transazioni nella mempool:");
+        for txid in mempool {
+            println!("{}", txid);
+        }
+    }
 
     Ok(())
 }
 
 fn main() {
-    main_result().unwrap();
+    if let Err(e) = main_result() {
+        eprintln!("Errore: {:?}", e);
+    }
 }
