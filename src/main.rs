cce87dc826179c7ce84a5c25791712d2d2f6e98b
lorban <lorenzo.bonax@gmail.com>
2023-11-24 20:23:58 +0100

    wrote code, compiles but doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 extern crate bitcoincore_rpc;
 
+mod minimal_rpc;
+
 use bitcoincore_rpc::{Auth, Client, Error, RpcApi};
 
-fn main_result() -> Result<(), Error> {
+async fn main_result() -> Result<(), Error> {
     let url = "http://127.0.0.1:18332".to_string();
     let username = "username".to_string(); 
     let password = "password".to_string(); 
-    let auth = Auth::UserPass(username, password);
+    let auth = Auth::UserPass(username.clone(), password.clone());
 
     let rpc = Client::new(&url, auth)?;
 
     let mempool = rpc.get_raw_mempool()?;
+    let result = minimal_rpc::get_raw_mempool_(&url, &username, &password); 
     
     if mempool.is_empty() {
         println!("La mempool è vuota.");
@@ -20,12 +23,15 @@ fn main_result() -> Result<(), Error> {
             println!("{}", txid);
         }
     }
+    result.await;
+    println!("BBBBBBB");
 
     Ok(())
 }
 
-fn main() {
-    if let Err(e) = main_result() {
+#[tokio::main]
+async fn main() {
+    if let Err(e) = main_result().await {
         eprintln!("Errore: {:?}", e);
     }
 }
