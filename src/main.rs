2342b6b6871293ff489579e26ae3108ee494faa0
lorban <lorenzo.bonax@gmail.com>
2023-12-28 13:34:43 +0100

    trying with hyper_util::legacy::Client

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,34 @@
 use base64::Engine;
-use hyper::Request;
-use serde::{Deserialize, Serialize};
-use serde_json::json;
 use bytes::Bytes;
-//use bytes::Buf; 
 use http_body_util::{BodyExt, Full};
 use hyper::header::{AUTHORIZATION, CONTENT_TYPE};
+use hyper::Request;
+use hyper_util::client::legacy::connect::HttpConnector;
 use hyper_util::rt::TokioIo;
+use hyper_util::rt::TokioExecutor;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use tokio::net::TcpStream;
-
+use hyper_util::client::legacy::Client;
 
 #[tokio::main]
 async fn main() {
     let response = send_json_rpc_request("getrawmempool", json!([])).await;
     match response {
         Ok(result) => {
-            let response_: Result<Vec<String>, ()> = match serde_json::from_value(result.result.unwrap()) {
-                Ok(response_inner) => response_inner,
-                Err(_) => {
-                    println!("Deserialization error");
-                    Err(())
-                }
-            };
+            let response_: Result<Vec<String>, ()> =
+                match serde_json::from_value(result.result.unwrap()) {
+                    Ok(response_inner) => response_inner,
+                    Err(_) => {
+                        println!("Deserialization error");
+                        Err(())
+                    }
+                };
             println!("The mempool is: {:?}", response_.unwrap());
         }
         Err(error) => {
             println!("Error: {:?}", error);
-        },
+        }
     }
 }
 
@@ -35,14 +37,15 @@ async fn send_json_rpc_request<T: for<'de> Deserialize<'de> + std::fmt::Debug>(
     params: serde_json::Value,
 ) -> Result<JsonRpcResult<T>, JsonRpcError> {
     let url = "http://127.0.0.1:18332".parse::<hyper::Uri>().unwrap();
-    let host = url.host().expect("uri has no host");
-    let port = url.port_u16().unwrap_or(18332);
-    let address = format!("{}:{}", host, port);
-    let stream = TcpStream::connect(address).await.unwrap();
-    //dbg!(&stream);
-    let io = TokioIo::new(stream);
-    let (mut sender, _conn) = hyper::client::conn::http1::handshake(io).await.unwrap();
+    //let host = url.host().expect("uri has no host");
+    //let port = url.port_u16().unwrap_or(18332);
+    //let address = format!("{}:{}", host, port);
+    //let stream = TcpStream::connect(address).await.unwrap();
+    //let io = TokioIo::new(stream);
+    //let (mut sender, _conn) = hyper::client::conn::http1::handshake(io).await.unwrap();
     let (username, password) = ("username", "password");
+    //let client: Client<HttpConnector, Full<>
+    let client: Client<_, Full<Bytes>> = Client::builder(TokioExecutor::new()).build_http();
     let request = JsonRpcRequest {
         jsonrpc: "2.0".to_string(),
         method: method.to_string(),
@@ -50,7 +53,7 @@ async fn send_json_rpc_request<T: for<'de> Deserialize<'de> + std::fmt::Debug>(
         id: 1,
     };
 
-    let request_body = serde_json::to_string(&request).unwrap(); 
+    let request_body = serde_json::to_string(&request).unwrap();
 
     let req = Request::builder()
         .method("POST")
@@ -70,19 +73,11 @@ async fn send_json_rpc_request<T: for<'de> Deserialize<'de> + std::fmt::Debug>(
     dbg!(&req);
 
     println!("A");
-    let response = sender
-        .send_request(req)
-        .await
-        .unwrap();
+    let response = client.request(req).await.unwrap();
 
     println!("B");
     let status = response.status();
-    let body = response
-        .into_body()
-        .collect()
-        .await
-        .unwrap()
-        .to_bytes();
+    let body = response.into_body().collect().await.unwrap().to_bytes();
     //let frame = body.collect();
     //let chunck = frame.
     //let body = hyper::body::to_bytes(body)
